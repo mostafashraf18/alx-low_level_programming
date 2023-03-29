d941081271af89b2d7f23a80f01487b8b917b6c7
mostafashraf18 <ma2963629@gmail.com>
2023-03-29 05:00:41 -0700

    final touch

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int nl = strlen(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[nl + i] = src[i];
+	}
 	dest[nl + i] = '\0';
 	return (dest);
 }
