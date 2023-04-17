e35c409492b204cec6a7aeb13baa9d2b4e7a0728
mostafashraf18 <ma2963629@gmail.com>
2023-04-17 12:45:04 +0200

    fix the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
