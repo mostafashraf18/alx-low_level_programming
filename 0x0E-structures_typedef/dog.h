3c408e4a697ff85ed91a4a4390cc43916668ccd7
mostafashraf18 <ma2963629@gmail.com>
2023-04-17 12:34:04 +0200

    fix the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,11 +10,10 @@
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
-
-struct dog {
+ struct dog {
 	char *name;
 	float age;
 	char *owner;
-};
+ };
 
 #endif
